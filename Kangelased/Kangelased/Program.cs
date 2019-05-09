9fb45e23afebf408e406e0df7b8e7b1d1d3559c7
Brandon <opilane@HP-CZC6297B8V>
2019-05-09 12:28:19 +0300

    Lahendus 3.ülesanne -- Loo Kangelased List

--- Kangelased/Kangelased/Program.cs
+++ Kangelased/Kangelased/Program.cs
@@ -8,6 +8,27 @@ namespace Kangelased
 {
     class Program
     {
+        static List<Kangelased> kangelased = new List<Kangelased>();
+        static void LaeKangelased(string Failinimi)
+        {
+            string[] failiAndmed = System.IO.File.ReadAllLines(Failinimi);
+            for (int i = 0; i < failiAndmed.Length; i++)
+            {
+                string[] nimiKoht = failiAndmed[i].Split(new char[] { '/' });
+                string nimi = nimiKoht[0].Trim();
+                string asukoht = nimiKoht[1].Trim();
+                if(nimi.EndsWith("*"))
+                {
+                    kangelased.Add(new Superkangelane(nimi.Trim(new char[] { '*' }), asukoht));
+                }
+                else
+                {
+                    kangelased.Add(new Kangelased(nimi, asukoht));
+                }
+            }
+
+        }
+
         static void Main(string[] args)
         {
 
