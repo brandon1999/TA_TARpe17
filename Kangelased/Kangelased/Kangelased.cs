6b606c8ecdb730e80cd767eebd116797baeda161
Brandon <opilane@HP-CZC6297B8V>
2019-05-09 11:41:28 +0300

    Lahenda esimene ülesanne - Loo kangelase klass.

---
+++ Kangelased/Kangelased/Kangelased.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangelased
+{
+    class Kangelased
+    {
+
+        private string _Nimi;
+        private string _Asukoht;
+
+        public Kangelased(string Nimi, string Asukoht) {
+
+            this.Nimi = Nimi;
+            this.Asukoht = Asukoht;
+        }
+        public string Nimi {
+            get { return _Nimi; }
+            set { _Nimi = value; }
+        }
+        public string Asukoht {
+            get { return _Asukoht; }
+            set { _Asukoht = value; }
+        }
+        public int Päästa(int Ohustatuid)
+        {
+            return (int)Math.Round(Ohustatuid * 95.0 / 100);
+        }
+        public override string ToString()
+        {
+            return Nimi + " hoiab " + Asukoht + " turavalisena.";
+        }
+    }
+}
